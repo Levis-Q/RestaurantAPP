ca507e5c560c77fa4fd56adbe8e34a0b7558e3df
Levis-Q <rodneytebogo17@gmail.com>
2023-01-25 15:47:55 +0200

    Added function to share data to food page

--- src/app/components/pages/food-page/food-page.component.ts
+++ src/app/components/pages/food-page/food-page.component.ts
@@ -6,19 +6,20 @@ import { Food } from 'src/app/shared/models/Food';
 @Component({
   selector: 'app-food-page',
   templateUrl: './food-page.component.html',
-  styleUrls: ['./food-page.component.css']
+  styleUrls: ['./food-page.component.css'],
 })
 export class FoodPageComponent implements OnInit {
   food!: Food;
 
-  constructor(activatedRoute:ActivatedRoute, foodService:FoodService) { 
-    activatedRoute.params.subscribe((params) => {
-      if(params.id)
-      this.food = foodService.getFoodById(params.id);
-    })
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private foodService: FoodService
+  ) {
+    this.activatedRoute.params.subscribe((params) => {
+      if (params.id) this.food = this.foodService.getFoodById(params.id);
+      console.log(this.food);
+    });
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FoodService } from '../services/food/food.service';
 import { Food } from '../shared/models/Food';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  foods: Food[] = [];
 
-  foods:Food[] = [];
-
-  constructor(private foodService:FoodService, activatedRoute:ActivatedRoute) {
+  constructor(
+    private foodService: FoodService,
+    activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
     activatedRoute.params.subscribe((params) => {
-      if(params.searchTerm)
-      this.foods = this.foodService.getAllFoodsBySeachTerm(params.searchTerm);
-      else if(params.tag)
-      this.foods = this.foodService.getALLFoodsByTag(params.tag);
-      else
-      this.foods = foodService.getAll();
-    })
-    
-   }
+      if (params.searchTerm)
+        this.foods = this.foodService.getAllFoodsBySeachTerm(params.searchTerm);
+      else if (params.tag)
+        this.foods = this.foodService.getALLFoodsByTag(params.tag);
+      else this.foods = foodService.getAll();
+    });
+  }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
+  goToPage(food: Food) {
+    console.log(food);
+    this.router.navigate(['food/' + food.id]);
+  }
 }
